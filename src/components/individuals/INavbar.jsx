6c8eb9e6c3090df2c59f1c9f72dc3b0317e45b57
Bryan <bryansanudo95@gmail.com>
2023-10-05 15:20:40 -0500

    navbar 2

--- src/components/individuals/INavbar.jsx
+++ src/components/individuals/INavbar.jsx
@@ -44,8 +44,8 @@ const INavbar = ({ setContent }) => {
 
   return (
     <>
-      <div className="fixed w-full h-24 bg-gray-600 text-white z-20  ">
-        <div className="flex justify-between gap-6 items-center max-w-[1600px] mx-auto px-3 h-full">
+      <div className="fixed w-full h-24 bg-gray-400 text-white z-20  ">
+        <div className="flex justify-between gap-6 items-center mx-6 lg:mx-10  px-3 h-full">
           {/* <div className="hidden lg:flex items-center flex-row justify-center bg-white rounded-lg p-1 "></div> */}
           <img
             src={logo}
@@ -77,15 +77,18 @@ const INavbar = ({ setContent }) => {
             src="/favicon.png"
             className="h-10 object-contain lg:hidden block"
           />
-          <button className="btn  btn-secondary " onClick={btnCompanies}>
-            companies
+          <button
+            className="link link-secondary hover:scale-105 duration-700 "
+            onClick={btnCompanies}
+          >
+            Looking For Employees?
           </button>
         </div>
       </div>
 
       <div
         className={`fixed w-full  text-white z-10 left-0 h-fit py-12 lg:hidden flex justify-center text-center text-2xl duration-500 ${
-          isMenuShown ? "top-16 rounded-b-2xl bg-gray-600 " : "top-[-100%]"
+          isMenuShown ? "top-16 rounded-b-2xl bg-gray-400 " : "top-[-100%]"
         }`}
       >
         <ul>
