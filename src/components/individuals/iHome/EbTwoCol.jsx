bbaa0613b6fbee48e5dce835bf8d9c33e8fa2e0e
Bryan <bryansanudo95@gmail.com>
2023-10-06 16:19:57 -0500

    eb3-visa

--- src/components/individuals/iHome/EbTwoCol.jsx
+++ src/components/individuals/iHome/EbTwoCol.jsx
@@ -3,13 +3,13 @@ import Tilt from "react-parallax-tilt";
 
 const EbTwoCol = () => {
   return (
-    <div className="w-full flex flex-col items-center justify mb-20 mt-10">
+    <div className="w-full flex flex-col items-center justify mb-20 mt-10 px-2">
       <p className={`${styles.titleText} mb-6`}>EB-3 Visa</p>
       <div className="flex items-center justify-center flex-col md:flex-row gap-10 lg:gap-20 ">
         <Tilt tiltReverse={true}>
-          <div className="flex flex-col justify-center items-center gap-4 max-w-[600px] p-10 shadow-md shadow-black rounded-3xl">
-            <p className="text-4xl font-bold">Process Length</p>
-            <p className="text-xl font-bold">Around 18 months</p>
+          <div className="flex flex-col justify-center items-center gap-4 max-w-[600px] p-4 md:p-10 shadow-md shadow-black rounded-3xl">
+            <p className={`${styles.titleText}`}>Process Length</p>
+            <p className="font-bold">Around 18 months</p>
             <p className={`${styles.sectionText}`}>
               The length of the processing times for the EB3 program varies and
               depends on the location (Longstanding Successcountry of origin)
@@ -21,9 +21,9 @@ const EbTwoCol = () => {
         </Tilt>
 
         <Tilt tiltReverse={true}>
-          <div className="flex flex-col justify-center items-center  gap-4 max-w-[600px] p-10 shadow-md shadow-black rounded-3xl">
-            <p className="text-4xl font-bold">Investment</p>
-            <p className="text-xl font-bold">$400-$1200</p>
+          <div className="flex flex-col justify-center items-center  gap-4 max-w-[600px] p-4 md:p-10 shadow-md shadow-black rounded-3xl">
+            <p className={`${styles.titleText}`}>Investment</p>
+            <p className="font-bold">$400-$1200</p>
             <p className={`${styles.sectionText}`}>
               In order to proceed with the Labor Certificate, compliance with
               regulations necessitates the advertisement of the position to U.S.
