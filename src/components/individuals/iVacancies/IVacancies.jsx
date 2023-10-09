e1185a324f367cc9432a45a49c76df1f38eea620
Bryan <bryansanudo95@gmail.com>
2023-10-09 14:29:09 -0500

    bug deploy

--- src/components/individuals/iVacancies/IVacancies.jsx
+++ src/components/individuals/iVacancies/IVacancies.jsx
@@ -9,7 +9,7 @@ import lestersLogo from "@/assets/partners/lesters-logo.png";
 import stoughtonLogo from "@/assets/partners/stoughton-logo.png";
 import subwayLogo from "@/assets/partners/subway-logo.png";
 import transportationLogo from "@/assets/partners/transportation-logo.png";
-import travelkuzLogo from "@/assets/partners/travelkuz-logo.png";
+import travelkuzLogo from "@/assets/partners/travelkuz-logo-bg.png";
 import wildernessLogo from "@/assets/partners/wilderness-logo.png";
 import kartsLogo from "@/assets/partners/karst-logo.png";
 
