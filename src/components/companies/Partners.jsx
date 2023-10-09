bcce4015920aa9f939ec2aae15d3b1161e7e5e07
Bryan <bryansanudo95@gmail.com>
2023-10-09 14:30:27 -0500

    a

--- src/components/companies/Partners.jsx
+++ src/components/companies/Partners.jsx
@@ -1,11 +1,3 @@
-import chulaLogo from "@/assets/partners/chula-logo.png";
-import lestersLogo from "@/assets/partners/lesters-logo.png";
-import stoughtonLogo from "@/assets/partners/stoughton-logo.png";
-import subwayLogo from "@/assets/partners/subway-logo.png";
-import transportationLogo from "@/assets/partners/transportation-logo.png";
-import travelkuzLogo from "@/assets/partners/travelkuz-logo.png";
-import wildernessLogo from "@/assets/partners/wilderness-logo.png";
-import { styles } from "@/styles";
 import CPartners from "./CPartners";
 
 const Partners = () => {
