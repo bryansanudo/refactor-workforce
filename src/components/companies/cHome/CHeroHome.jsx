06a1a72fce96885efa5b654cdc6647dca4798af9
Bryan <bryansanudo95@gmail.com>
2023-10-11 11:45:02 -0500

    hero companies

--- src/components/companies/cHome/CHeroHome.jsx
+++ src/components/companies/cHome/CHeroHome.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-
 const CHeroHome = () => {
   return (
     <>
       <div
         className="hero min-h-[100vh]"
         style={{
-          backgroundImage:
-            "url(https://static.wixstatic.com/media/863055_797b59f1342445a19f07a0cee65d9812~mv2.jpg/v1/fill/w_1652,h_671,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/863055_797b59f1342445a19f07a0cee65d9812~mv2.jpg)",
+          backgroundImage: "url(/companies-home.jpg)",
         }}
       >
         <div className="hero-overlay bg-black opacity-50" />
