664d6c856b049a43ad49831cbb48a107b5aa3104
Bryan <bryansanudo95@gmail.com>
2023-10-12 14:09:47 -0500

    borders

--- src/components/companies/CPartners.jsx
+++ src/components/companies/CPartners.jsx
@@ -15,35 +15,30 @@ const CPartners = () => {
           <h1 className={`${styles.cTitleText}`}>
             Our Trusted Employer Sponsors
           </h1>
-          {/* <p className="text-base md:text-lg lg:text-xl text-center text-gray-600 font-normal xl:w-10/12 xl:mx-auto">
-            Our success has come from being committed to the property and
-            investing in the development of the product to maximize sales. At
-            the same time and maintaining the int
-          </p> */}
         </div>
         <div className="my-10 px-15 flex flex-wrap">
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b md:border-b xl:border-r md:border-r  border-secondary xl:pb-10 pb-16 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pb-10 pb-16 items-center px-6 md:px-0">
             <img src={chulaLogo} className="w-[170px]" alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b md:border-b xl:border-r md:border-r border-secondary xl:pb-10 pb-16 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pb-10 pb-16 items-center px-6 md:px-0">
             <img src={lestersLogo} className="w-[130px] " alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b md:border-b border-secondary xl:pb-10 pb-16 pt-4 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pb-10 pb-16 pt-4 items-center px-6 md:px-0">
             <img src={stoughtonLogo} className="w-[250px] " alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center md:border-b xl:border-b md:border-l xl:border-l border-secondary xl:pb-10 pb-16 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pb-10 pb-16 items-center px-6 md:px-0">
             <img src={subwayLogo} className="w-[250px] " alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r md:border-r border-secondary xl:pt-10 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pt-10 items-center px-6 md:px-0">
             <img src={transportationLogo} className="w-[200px] " alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r md:border-r border-secondary xl:pt-10 items-center px-6 md:px-0">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pt-10 items-center px-6 md:px-0">
             <img src={travelkuzLogo} className="w-[250px] " alt="" />
           </div>
           <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pt-10 lg:pt-10 md:pt-2 pt-16 px-6 md:px-0">
             <img src={wildernessLogo} className="w-[200px] " alt="" />
           </div>
-          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-l md:border-l border-secondary xl:pt-10 lg:pt-10 md:pt-2 pt-16"></div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center  xl:pt-10 lg:pt-10 md:pt-2 pt-16"></div>
         </div>
       </div>
     </>
